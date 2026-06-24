c4f5e35d7edec0a771d6aa8a17748eedcc7334a0
Mohit Kumar <mohitkumar2214030@gmail.com>
2026-06-24 18:39:20 +0000

    I learn about variables

---
+++ javascript/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 13456
+let emailAddress = "mohit@1234.com"
+var accountPass = "Mohit@8212"
+accountCity = "Varanasi"
+
+
+emailAddress = "mohit@.com"
+accountPass = "M1234"
+accountCity = "Bhadohi"
+let accountState;
+
+console.table([accountId,emailAddress,accountPass,accountCity,accountState]);
+
+/*
+    donot use var keyword
+    because of issue in block scope and functional scope
+*/
