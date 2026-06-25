a961f42dd2077265bdcc1b7a8f1e9395c0d5df6f
Mohit Kumar <mohitkumar2214030@gmail.com>
2026-06-25 05:37:16 +0000

    Data types

---
+++ javascript/02_Data_types.js
@@ -0,0 +1,23 @@
+"use stricr" // treat all js code as a newer version
+
+//alert( 3 + 5)  this line will not work because we are usig node js , not browser
+
+console.log(9 + 1)
+
+let userName = "Mohit"
+let age = 22
+let isLogedIn = true
+let state = null
+
+// Number => 2 to power 53
+// bigInt 
+// string = ""
+// boolean = true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined's data type is undefined
+console.log(typeof null); // null's data type is object
